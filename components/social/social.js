fdf1fb6f2e286d869e11fb736b3c1d1b03d0cf25
NotHiaki <celsohenrique367@gmail.com>
2023-07-23 18:09:00 -0300

    social buttons links

--- components/social/social.js
+++ components/social/social.js
@@ -2,7 +2,7 @@ import style from './social.module.css';
 
 function Social(props){
     return (
-    <a href={props.lk} >
+    <a href={props.lk} target="_blank">
         <img src={props.src} className={style.Social}></img>
     </a>
     );
